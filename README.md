ab416e9091664fc04147e3678dc47a000f165ac1
Chris Wilson <christopher.e.wilson@gmail.com>
2013-08-04 14:36:58 -0400

    v0.1

--- README.md
+++ README.md
@@ -1,15 +1,10 @@
 a_to_z
 ======
 
-Typography displays typically require sentences that use every letter in the English alphabet. The most canonical of these is
-"The quick brown fox jumped over the lazy dog." Google Fonts uses "Grumpy wizards make toxic brew for the evil Queen and Jack."
+Typography displays typically require sentences that use every letter in the English alphabet. The most canonical of these is "The quick brown fox jumped over the lazy dog." Google Fonts uses "Grumpy wizards make toxic brew for the evil Queen and Jack."
 
 This simple browser game invites you to create your own sentences that use every letter while remaining as succinct as possible.
 
-    
-
-
-# Source
-The list of words for suggestions is drawn from [The Scrabble Dictionary](https://encrypted.google.com/search?q=scrabble+dictionary+txt). 
-
+# Word validation
+The game only allows words that are in the ```2od12inf.txt``` list in [Kevin's Word List](http://wordlist.sourceforge.net/). The lookup algorithm, which uses a trie structure, comes courtesy of [John Resig](http://ejohn.org/blog/revised-javascript-dictionary-search/).
 
---
+++ js/utils.js
@@ -0,0 +1,70 @@
+function isWord( word, cur ) {
+    cur = cur || dict;
+
+	for ( var node in cur ) {
+		if ( word.indexOf( node ) === 0 ) {
+			var val = typeof cur[ node ] === "number" && cur[ node ] ?
+				dict.$[ cur[ node ] ] :
+				cur[ node ];
+
+			if ( node.length === word.length ) {
+				return val === 0 || val.$ === 0;
+
+			} else {
+				return isWord( word.slice( node.length ), val );
+			}
+		}
+	}
+	return false;
+}
+
+function setEndOfContenteditable(contentEditableElement)
+{
+    var range,selection;
+    if(document.createRange)//Firefox, Chrome, Opera, Safari, IE 9+
+    {
+        range = document.createRange();//Create a range (a range is a like the selection but invisible)
+        range.selectNodeContents(contentEditableElement);//Select the entire contents of the element with the range
+        range.collapse(false);//collapse the range to the end point. false means collapse to end rather than the start
+        selection = window.getSelection();//get the selection object (allows you to change selection)
+        selection.removeAllRanges();//remove any selections already made
+        selection.addRange(range);//make the range you have just created the visible selection
+    }
+    else if(document.selection)//IE 8 and lower
+    { 
+        range = document.body.createTextRange();//Create a range (a range is a like the selection but invisible)
+        range.moveToElementText(contentEditableElement);//Select the entire contents of the element with the range
+        range.collapse(false);//collapse the range to the end point. false means collapse to end rather than the start
+        range.select();//Select the range (make it the visible selection
+    }
+}
+
+/*
+** Returns the caret (cursor) position of the specified text field.
+** Return value range is 0-oField.value.length.
+** http://stackoverflow.com/questions/2897155/get-cursor-position-within-an-text-input-field
+*/
+
+function getCaretPosition(editableDiv) {
+    var caretPos = 0, containerEl = null, sel, range;
+    if (window.getSelection) {
+        sel = window.getSelection();
+        if (sel.rangeCount) {
+            range = sel.getRangeAt(0);
+            if (range.commonAncestorContainer.parentNode == editableDiv) {
+                caretPos = range.endOffset;
+            }
+        }
+    } else if (document.selection && document.selection.createRange) {
+        range = document.selection.createRange();
+        if (range.parentElement() == editableDiv) {
+            var tempEl = document.createElement("span");
+            editableDiv.insertBefore(tempEl, editableDiv.firstChild);
+            var tempRange = range.duplicate();
+            tempRange.moveToElementText(tempEl);
+            tempRange.setEndPoint("EndToEnd", range);
+            caretPos = tempRange.text.length;
+        }
+    }
+    return caretPos;
+}
